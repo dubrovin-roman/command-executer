bd67358c73733dce2c33f4987477b4509053159d
Roman <dubrovin.skfo@gmail.com>
2023-08-20 13:53:35 +0300

    обработка ввода

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,9 @@
+import { PromptService } from "./core/prompt/prompt.service";
+
 export class App {
-  run() {
-    console.log("Done");
+  async run() {
+    const res = await (new PromptService()).input<number>("Введите пароль", "number");
+    console.log(res);
   }
 }
 
